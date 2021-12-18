5aeaf6015df249ffa2006feb081507a58c627b8f
Nhu Phan <nb.phan@hotmail.com>
2021-12-18 13:23:27 -0700

    Add excel sheet column title solution function template and setup tests for solution.

---
+++ src/excelSheetColumnTitle/excelSheetColumnTitle.ts
@@ -0,0 +1,15 @@
+/**
+ * Problem: Given an integer columnNumber, return its corresponding column title
+ *  as it appears in an Excel sheet.
+ *
+ *  Constraints
+ *  1 <= columnNumber <= 231 - 1
+ *
+ * @param columnNumber
+ */
+
+function convertToTitle(columnNumber: number): string {
+  return "";
+}
+
+export default convertToTitle;
---
+++ tests/excelSheetColumnTitle.test.ts
@@ -0,0 +1,25 @@
+import { expect } from "chai";
+import "mocha";
+import convertToTitle from "../src/excelSheetColumnTitle/excelSheetColumnTitle";
+
+describe("excel sheet column title solution tests", () => {
+  it("should return A if input column number is 1", () => {
+    let result = convertToTitle(1);
+    expect(result).to.equal("A");
+  });
+
+  it("should return AB if input column number is 28", () => {
+    let result = convertToTitle(28);
+    expect(result).to.equal("AB");
+  });
+
+  it("should return ZY if input column number is 701", () => {
+    let result = convertToTitle(701);
+    expect(result).to.equal("ZY");
+  });
+
+  it("should return FXSHRXW if input column number is 2147483647", () => {
+    let result = convertToTitle(2147483647);
+    expect(result).to.equal("FXSHRXW");
+  });
+});
