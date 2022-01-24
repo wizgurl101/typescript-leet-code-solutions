8e6f3cb39c02029cf8ab15bd91034702ba5d75fa
Nhu Phan <nb.phan@hotmail.com>
2022-01-24 04:17:55 -0700

    Implement execel sheet coulmn title problem solution and add .skip to tests for past problems

--- src/excelSheetColumnTitle/excelSheetColumnTitle.ts
+++ src/excelSheetColumnTitle/excelSheetColumnTitle.ts
@@ -8,10 +8,35 @@
  *  1 <= columnNumber <= 2^31 - 1
  *
  *  think of modulus and multiple of 26 (because there is 26 letters)
+ *
+ * 1. Create an empty string for storing the characters.
+ * 2. Run a loop while n is positive.
+ *     - Subtract 1 from n.
+ *     - Get current character by doing modulo of  n by 26.
+ *     - Divide n by 26.
+ * 3. Now reverse the result string because we have found characters from right to left.
+ * 4. Return the reversed string.
+ *
  */
 
+function getChar(num: number) {
+  let start = "A".charCodeAt(0);
+  return String.fromCharCode(start + --num);
+}
+
 function convertToTitle(columnNumber: number): string {
-  return "";
+  let result = "";
+  let currentCharacterNumber = 0;
+
+  while (columnNumber > 0) {
+    currentCharacterNumber = columnNumber % 26 === 0 ? 26 : columnNumber % 26;
+
+    result = getChar(currentCharacterNumber) + result;
+
+    columnNumber = (columnNumber - currentCharacterNumber) / 26;
+  }
+
+  return result;
 }
 
 export default convertToTitle;
--- tests/validParenthese.test.ts
+++ tests/validParenthese.test.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { validParenthese } from "../src/validParentheses/validParenthese";
 import "mocha";
 
-describe("valid parenthese solution tests", () => {
+describe.skip("valid parenthese solution tests", () => {
   it("should return true if input given is ()", () => {
     let result = validParenthese("()");
     expect(result).to.equal(true);
