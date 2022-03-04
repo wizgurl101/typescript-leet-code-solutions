dc9e5fe80154aaa8d45c824a20e55bc49cf55381
Nhu Phan <nb.phan@hotmail.com>
2022-03-04 08:29:17 -0700

    solved lowest common ancestor

---
+++ src/BST/lowestCommonAncestor.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null
+): TreeNode | null {
+  let current = root;
+
+  while (current) {
+    if (p!.val > current.val && q!.val > current.val) {
+      current = current.right;
+    } else if (p!.val < current.val && q!.val < current.val) {
+      current = current.left;
+    } else {
+      return current;
+    }
+  }
+}
