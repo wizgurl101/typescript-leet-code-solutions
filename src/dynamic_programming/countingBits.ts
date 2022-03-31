e86f0ce09dd4f48d817f88ba986f2d876581a445
Nhu Phan <nb.phan@hotmail.com>
2022-03-30 19:51:42 -0600

    solved counting bits

--- src/dynamic_programming/countingBits.ts
+++ src/dynamic_programming/countingBits.ts
@@ -1,20 +1,20 @@
 // https://leetcode.com/problems/counting-bits/
 export function countBits(n: number): number[] {
-    let count: number[] = [];
+  let count: number[] = [];
 
-    for(let i = 0; i < n + 1; i++) {
-        count.push(0)
+  for (let i = 0; i < n + 1; i++) {
+    count.push(0);
+  }
+
+  let offset = 1;
+
+  for (let i = 1; i < n + 1; i++) {
+    if (offset * 2 === i) {
+      offset = i;
     }
 
-    let offset = 1;
+    count[i] = 1 + count[i - offset];
+  }
 
-    for(let i = 0; i < n + 1; i++) {
-        if (offset * 2 === i) {
-            offset = i
-        }
-
-        count[i] = 1 + count[i - offset]
-    }
-
-    return count
-};
+  return count;
+}
