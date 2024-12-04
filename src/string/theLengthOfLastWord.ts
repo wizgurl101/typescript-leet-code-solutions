539d1bbb88cf07ccf120fe510d24c783596b706d
wizgurl101 <nb.phan@hotmail.com>
2024-12-04 11:31:56 -0700

    Update theLengthOfLastWord.ts

--- src/string/theLengthOfLastWord.ts
+++ src/string/theLengthOfLastWord.ts
@@ -4,33 +4,50 @@
  */
 
 export const LenghtOfLastWord = (s: string): number => {
-  let words = [];
-  let current_word: string[] = [];
+  let length = 0;
 
-  for (let i = 0; i < s.length; i++) {
-    if (i === s.length - 1 && s[i] !== ' ') {
-      current_word.push(s[i]);
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (i !== s.length - 1 && s[i] === ' ' && length !== 0) {
+      return length;
     }
 
-    if (s[i] === ' ' || i === s.length - 1) {
-      if (current_word.length !== 0) {
-        let current_word_string = turnToString(current_word);
-        words.push(current_word_string);
-        current_word = [];
-      }
-    } else {
-      current_word.push(s[i]);
+    if (s[i] !== '') {
+      length++;
     }
   }
 
-  const lastWord = words.pop();
-  return lastWord ? lastWord.length : 0;
+  return length;
 };
 
-const turnToString = function (listOfwords: string[]): string {
-  let word = '';
-  for (let i = 0; i < listOfwords.length; i++) {
-    word = word + listOfwords[i];
-  }
-  return word;
-};
+// first solution
+// export const LenghtOfLastWord = (s: string): number => {
+//   let words = [];
+//   let current_word: string[] = [];
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (i === s.length - 1 && s[i] !== ' ') {
+//       current_word.push(s[i]);
+//     }
+
+//     if (s[i] === ' ' || i === s.length - 1) {
+//       if (current_word.length !== 0) {
+//         let current_word_string = turnToString(current_word);
+//         words.push(current_word_string);
+//         current_word = [];
+//       }
+//     } else {
+//       current_word.push(s[i]);
+//     }
+//   }
+
+//   const lastWord = words.pop();
+//   return lastWord ? lastWord.length : 0;
+// };
+
+// const turnToString = function (listOfwords: string[]): string {
+//   let word = '';
+//   for (let i = 0; i < listOfwords.length; i++) {
+//     word = word + listOfwords[i];
+//   }
+//   return word;
+// };
